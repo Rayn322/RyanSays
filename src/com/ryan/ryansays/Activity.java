5cc6ff87c627b8e3a3c600a54beb2dbade4eca09
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2020-12-22 14:55:30 -0800

    Initial Commit

---
+++ src/com/ryan/ryansays/Activity.java
@@ -0,0 +1,12 @@
+package com.ryan.ryansays;
+
+public enum Activity {
+    DRINKPOTION
+//    JUMP,
+//    JUMPOFF,
+//    LASTONESTANDING,
+//    PLACEBLOCK,
+//    PUNCHSOMEONE,
+//    STANDSTILL,
+//    TYPECHARACTER
+}
